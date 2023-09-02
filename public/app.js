bdd22a2d8e57984195898ab16961c8bc402ad8d4
Iamneo <techadmin@iamneo.ai>
2023-09-02 11:17:02 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,117 +1,61 @@
-// Initial game state
-let cells = ['', '', '', '', '', '', '', '', ''];
-let currentPlayer = 'X';
-let result = document.querySelector('.result');
-let btns = document.querySelectorAll('.btn');
-let conditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-
-// Function to handle player moves
-const ticTacToe = (element, index) => {
-    // Your game logic here
-
-    /*
-    **Part 1: Winning Conditions (Add your code here)**
-
-    1. Implement the logic to check for winning conditions using the 'conditions' array.
-    2. Display a winning message in the 'result' element when a player wins.
-    3. Disable all buttons after a win.
-    */
-
-    // Your code to update the game state and check for a win
-    // ...
-
-    // Your code to display the current player's turn
-    // ...
-
-    // Your code to handle button and cell interactions
-    // ...
-};
-
-    /*
-    **Part 2: Reset Function (Add your code here)**
-
-    1. Implement a new function that resets the game to its initial state.
-    2. Ensure the 'cells', 'btns', and 'currentPlayer' variables are reset.
-    3. Update the 'result' element to indicate the current player's turn.
-    4. Re-enable all buttons for a new game.
-    */
-
-// Function to reset the game
-const resetGame = () => {
-    // Your code to reset the game state
-    // ...
-
-    // Your code to update the 'result' element
-    // ...
-
-    // Your code to re-enable buttons
-    // ...
-};
-
-btns.forEach((btn, i) => {
-    btn.addEventListener('click', () => ticTacToe(btn, i));
-});
-
-document.querySelector('#reset').addEventListener('click', resetGame);
-
-// const cells = document.querySelectorAll('[data-cell]');
-// const result = document.getElementById('result');
-const resetButton = document.getElementById('reset');
-// let currentPlayer = 'X';
+const cells = document.querySelectorAll("[data-cell]");
+const result = document.getElementById("result");
+const resetButton = document.getElementById("reset");
+let currentPlayer = "X";
 let gameOver = false;
 
-cells.forEach(cell => {
-    cell.addEventListener('click', handleCellClick);
+cells.forEach((cell) => {
+  cell.addEventListener("click", handleCellClick);
 });
 
-resetButton.addEventListener('click', resetGame);
+resetButton.addEventListener("click", resetGame);
 
 function handleCellClick(e) {
-    const cell = e.target;
-    
-    if (cell.textContent || gameOver) return;
-    
-    cell.textContent = currentPlayer;
-    
-    if (checkWin()) {
-        result.textContent = `Player ${currentPlayer} wins!`;
-        gameOver = true;
-    } else if ([...cells].every(cell => cell.textContent)) {
-        result.textContent = "It's a draw!";
-        gameOver = true;
-    } else {
-        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-        result.textContent = `Player ${currentPlayer}'s Turn`;
-    }
+  const cell = e.target;
+
+  if (cell.textContent || gameOver) return;
+
+  cell.textContent = currentPlayer;
+
+  if (checkWin()) {
+    result.textContent = `Player ${currentPlayer} wins!`;
+    gameOver = true;
+  } else if ([...cells].every((cell) => cell.textContent)) {
+    result.textContent = "It's a draw!";
+    gameOver = true;
+  } else {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    result.textContent = `Player ${currentPlayer}'s Turn`;
+  }
 }
 
 function checkWin() {
-    const winningCombinations = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-        [0, 4, 8], [2, 4, 6]             // Diagonals
-    ];
-    
-    return winningCombinations.some(combination => {
-        const [a, b, c] = combination;
-        return cells[a].textContent && cells[a].textContent === cells[b].textContent && cells[a].textContent === cells[c].textContent;
-    });
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], // Rows
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], // Columns
+    [0, 4, 8],
+    [2, 4, 6], // Diagonals
+  ];
+
+  return winningCombinations.some((combination) => {
+    const [a, b, c] = combination;
+    return (
+      cells[a].textContent &&
+      cells[a].textContent === cells[b].textContent &&
+      cells[a].textContent === cells[c].textContent
+    );
+  });
 }
 
 function resetGame() {
-    cells.forEach(cell => {
-        cell.textContent = '';
-    });
-    currentPlayer = 'X';
-    gameOver = false;
-    result.textContent = 'Player X\'s Turn';
+  cells.forEach((cell) => {
+    cell.textContent = "";
+  });
+  currentPlayer = "X";
+  gameOver = false;
+  result.textContent = "Player X's Turn";
 }
